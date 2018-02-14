23ee9189f810fbef28e1afe34daa3e859d58b9a9
Clement Delafargue <clement@delafargue.name>
2018-02-14 12:04:10 +0100

    Improve ServiceRepository and wire controller

--- app/controllers/ServiceController.scala
+++ app/controllers/ServiceController.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 import scala.concurrent.{ExecutionContext, Future}
 import play.api.libs.json._
 
+import models.entities.ServiceInstances.completeServiceWrites
 import models.repositories.ServiceRepository
 
 @Singleton
@@ -16,7 +17,9 @@ class ServiceController @Inject()(
   )(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
     def listServices = authed.async { r =>
-      serviceRepo.listAll.map(x => Ok(r.auth.toString))
+      serviceRepo
+        .list(r.auth.userId)
+        .map(x => Ok(r.auth.toString))
     }
 
     type ServiceId = java.util.UUID
@@ -24,7 +27,6 @@ class ServiceController @Inject()(
     implicit val scdFormats = Json.format[ServiceCreationData]
 
     def createService = authed.async(parse.json[ServiceCreationData]) { implicit request =>
-      println(request.body.toString)
       Future.successful(NotImplemented)
     }
 
@@ -32,11 +34,18 @@ class ServiceController @Inject()(
       NotImplemented
     }
 
-    def getService(id: ServiceId) = authed { request =>
-      NotImplemented
+    def getService(id: ServiceId) = authed.async { request =>
+      serviceRepo
+        .get(id, request.auth.userId)
+        .map({
+          case Some(cs) => Ok(Json.toJson(cs))
+          case None => NotFound
+        })
     }
 
-    def deleteService(id: ServiceId) = authed { request =>
-      NotImplemented
+    def deleteService(id: ServiceId) = authed.async { request =>
+      serviceRepo
+        .delete(id, request.auth.userId)
+        .map(_ => NoContent)
     }
 }
--- app/models/repositories/ServiceRepository.scala
+++ app/models/repositories/ServiceRepository.scala
@@ -23,6 +23,16 @@ class ServiceRepository @Inject()(dbapi: DBApi)(implicit ec: models.DatabaseExec
 
   private val checkColumns = columns[Check].map(c => s""""${tableName[Check]}".${c.name}""").mkString(",")
 
+  private def completeParser = parser[Service]() ~ parser[Check]()
+  private def gatherResults(lines: Seq[Service~Check]): Seq[CompleteService] = {
+    lines
+      .map({ case (s ~ e) => s -> e})
+      .groupBy(_._1)
+      .map({ case (s, vs) => CompleteService(s, vs.map(_._2))})
+      .toList
+  }
+
+
   def listAll: Future[Seq[CompleteService]] = Future(db.withConnection { implicit connection =>
     val x = s"""
       select ${columnList[Service](None)},
@@ -30,52 +40,55 @@ class ServiceRepository @Inject()(dbapi: DBApi)(implicit ec: models.DatabaseExec
       from "${tableName[Service]}"
       inner join "${tableName[Check]}" using(service_id)
     """
-    SQL(x)
-      .as((parser[Service]() ~ parser[Check]()).*)
-      .map({ case (s ~ e) => s -> e})
-      .groupBy(_._1)
-      .map({ case (s, vs) => CompleteService(s, vs.map(_._2))})
-      .toList
+  
+    gatherResults(SQL(x).as(completeParser.*))
   })(ec)
 
   def list(userId: UUID): Future[Seq[CompleteService]] = Future(db.withConnection { implicit connection =>
     var query = s"""
       select ${columnList[Service](None)},
-             ${columnList[Check](None)}
+             ${checkColumns}
       from ${tableName[Service]}
       inner join ${tableName[Check]} using(service_id)
-      where user_id = ${userId.toString}
+      where user_id = {userId}
     """
-    SQL(query)
-      .as((parser[Service]() ~ parser[Check]()).*)
-      .map({ case (s ~ e) => s -> e})
-      .groupBy(_._1)
-      .map({ case (s, vs) => CompleteService(s, vs.map(_._2))})
-      .toList
+    gatherResults(
+      SQL(query)
+        .on(
+          'userId -> userId.toString
+        )
+        .as(completeParser.*)
+    )
   })(ec)
 
-  def get(serviceId: UUID): Future[Seq[CompleteService]] = Future(db.withConnection { implicit connection =>
+  def get(serviceId: UUID, userId: UUID): Future[Option[CompleteService]] = Future(db.withConnection { implicit connection =>
     var query = s"""
       select ${columnList[Service](None)},
-             ${columnList[Check](None)}
+             ${checkColumns}
       from ${tableName[Service]}
       inner join ${tableName[Check]} using(service_id)
-      where service_id = ${serviceId.toString}
+      where service_id = {serviceId} and user_id = {userId}
     """
-    SQL(query)
-      .as((parser[Service]() ~ parser[Check]()).*)
-      .map({ case (s ~ e) => s -> e})
-      .groupBy(_._1)
-      .map({ case (s, vs) => CompleteService(s, vs.map(_._2))})
-      .toList
+    gatherResults(
+      SQL(query)
+        .on(
+          'serviceId -> serviceId.toString,
+          'userId -> userId.toString
+        )
+        .as(completeParser.*)
+    ).headOption
   })(ec)
 
-  def delete(serviceId: UUID): Future[Unit] = Future(db.withConnection { implicit connection =>
+  def delete(serviceId: UUID, userId: UUID): Future[Unit] = Future(db.withConnection { implicit connection =>
     var query = s"""
       delete from ${tableName[Service]}
-      where service_id = ${serviceId.toString}
+      where service_id = {serviceId} and user_id = {userId}
     """
     SQL(query)
+      .on(
+        'serviceId -> serviceId.toString,
+        'userId -> userId.toString
+      )
     ()
   })(ec)
 
