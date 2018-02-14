cf60ea4359de10ff439bc5dde41b910208f23b46
Clement Delafargue <clement@delafargue.name>
2018-02-14 16:11:15 +0100

    Fix magnolia generation

--- app/models/http/PaginatedService.scala
+++ app/models/http/PaginatedService.scala
@@ -3,7 +3,7 @@ package models.http
 import play.api.libs.json._
 
 import models.entities.CompleteService
-import models.entities.ServiceInstances._
+import models.entities.ServiceInstances.completeServiceWrites
 
 case class PaginatedService(
   items: Seq[CompleteService]
