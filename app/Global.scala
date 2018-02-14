4af847e9429e50fc13463ef6fd9d2920d689c145
Alexandre Berthaud <alexandre@berthaud.me>
2018-02-14 11:00:52 +0100

    Make scheduler optional, via an env var

--- app/Global.scala
+++ app/Global.scala
@@ -6,6 +6,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import play.api._
 import play.api.libs.concurrent.Akka
+import play.api.Configuration
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 
@@ -16,13 +17,17 @@ import models.AmqpClient
 class Global @Inject() (
   app: Application,
   actorSystem: ActorSystem,
-  scheduler: Scheduler
+  scheduler: Scheduler,
+  conf: Configuration
   )(implicit ec: ExecutionContext) {
 
   Logger.info("ON START")
-  actorSystem.scheduler.schedule(10.seconds, 1.minutes) {
-    //scheduler.httpTick()
-  }
+  if(conf.get[Boolean]("scheduler.enabled")) {
+    Logger.info("Scheduler is enabled")
+    actorSystem.scheduler.schedule(10.seconds, 1.minutes) {
+      scheduler.httpTick()
+    }
+  } else Logger.info("Scheduler is disabled")
 }
 
 class GlobalModule extends AbstractModule {
